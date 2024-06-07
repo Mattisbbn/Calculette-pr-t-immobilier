593eff43f5371ea24b0e9edaf8ebcab60003a8f4
Mattisbbn <mattisbbn@gmail.com>
2024-06-07 16:11:16 +0200

    Update

--- script/script.js
+++ script/script.js
@@ -1,28 +1,27 @@
 const form = document.querySelector("form");
+const tbody = document.querySelector("tbody");
 const pdfButton = document.querySelector("#pdfButton");
+const inputs = document.querySelectorAll("input")
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const tbody = document.querySelector("tbody");
+form.addEventListener("submit", drawTable)
+pdfButton.addEventListener("click", generatePDF);
 
-  let borrowed_amount = parseInt(
-    document.querySelector("#borrowed_amount").value
-  );
-  let nominal_rate = parseFloat(document.querySelector("#nominal_rate").value);
-  let repaymentTermInMonth = parseInt(
-    document.querySelector("#repayment_term").value * 12
-  );
-  tbody.innerHTML = "";
+function drawTable(event){
+event.preventDefault();
 
-  let interestPerMonth = nominal_rate / 12 / 100;
-  let remainingBalance = borrowed_amount;
-  let balance = borrowed_amount;
+let borrowed_amount = parseInt(document.querySelector("#borrowed_amount").value);
+let nominal_rate = parseFloat(document.querySelector("#nominal_rate").value);
+let repaymentTermInMonth = parseInt(document.querySelector("#repayment_term").value * 12);
+let interestPerMonth = nominal_rate / 12 / 100;
+let remainingBalance = borrowed_amount;
+let balance = borrowed_amount;
+
+tbody.innerHTML = "";
+
   for (let i = 1; i <= repaymentTermInMonth; i++) {
+
     let interestOfMonth = remainingBalance * interestPerMonth;
-    let monthlyDue =
-      borrowed_amount *
-      ((interestPerMonth * (1 + interestPerMonth) ** repaymentTermInMonth) /
-        ((1 + interestPerMonth) ** repaymentTermInMonth - 1));
+    let monthlyDue = borrowed_amount * ((interestPerMonth * (1 + interestPerMonth) ** repaymentTermInMonth) / ((1 + interestPerMonth) ** repaymentTermInMonth - 1));
     let amortization = monthlyDue - interestOfMonth;
 
     remainingBalance -= amortization;
@@ -38,21 +37,21 @@ form.addEventListener("submit", function (event) {
     balance = remainingBalance;
     tbody.innerHTML += row;
   }
-});
+   pdfButton.style.display = "block"
 
-pdfButton.addEventListener("click", generatePDF);
+}
 
 function generatePDF() {
-  const { jsPDF } = window.jspdf;
-  const doc = new jsPDF();
-
-  doc.autoTable({
-    html: "table",
-    theme: "striped",
-    headStyles: { fillColor: [0, 0, 255] },
-    styles: { fontSize: 10, cellPadding: 3 },
-    margin: { top: 20 },
-  });
-
-  doc.save("a4.pdf");
-}
+    const { jsPDF } = window.jspdf;
+    const doc = new jsPDF();
+  
+    doc.autoTable({
+      html: "table",
+      theme: "striped",
+      headStyles: { fillColor: [0, 0, 255] },
+      styles: { fontSize: 10, cellPadding: 3 },
+      margin: { top: 20 },
+    });
+  
+    doc.save("a4.pdf");
+  }
