1764e27c629128ec43ecde0faf5759a52358f419
Mattisbbn <mattisbbn@gmail.com>
2024-06-19 09:11:39 +0200

    Update

--- script/script.js
+++ script/script.js
@@ -1,36 +1,37 @@
 const form = document.querySelector("form");
 const tbody = document.querySelector("tbody");
 const pdfButton = document.querySelector("#pdfButton");
-const inputs = document.querySelectorAll("input")
+const inputs = document.querySelectorAll("input");
 
-const borrowed_amount_input = document.querySelector("#borrowed_amount")
-const nominal_rate_input = document.querySelector("#nominal_rate")
-const repayment_term_input = document.querySelector("#repayment_term")
+const borrowed_amount_input = document.querySelector("#borrowed_amount");
+const nominal_rate_input = document.querySelector("#nominal_rate");
+const repayment_term_input = document.querySelector("#repayment_term");
 
-form.addEventListener("submit", drawTable)
+form.addEventListener("submit", drawTable);
 pdfButton.addEventListener("click", generatePDF);
 
-function drawTable(event){
-event.preventDefault();
-checkNominalRate()
-const borrowed_amount = parseInt(borrowed_amount_input.value);
-const nominal_rate = parseFloat(nominal_rate_input.value);
-const repaymentTermInMonth = parseInt(repayment_term_input.value * 12);
+function drawTable(event) {
+  event.preventDefault();
+  const borrowed_amount = parseInt(borrowed_amount_input.value);
+  const nominal_rate = parseFloat(nominal_rate_input.value);
+  const repaymentTermInMonth = parseInt(repayment_term_input.value * 12);
 
+  let interestPerMonth = nominal_rate / 12 / 100;
+  let remainingBalance = borrowed_amount;
+  let balance = borrowed_amount;
+  if (checkNominalRate() == true && checkBorrowedAmount() == true) {
+    tbody.innerHTML = "";
 
-let interestPerMonth = nominal_rate / 12 / 100;
-let remainingBalance = borrowed_amount;
-let balance = borrowed_amount;
+    for (let i = 1; i <= repaymentTermInMonth; i++) {
+      let interestOfMonth = remainingBalance * interestPerMonth;
+      let monthlyDue =
+        borrowed_amount *
+        ((interestPerMonth * (1 + interestPerMonth) ** repaymentTermInMonth) /
+          ((1 + interestPerMonth) ** repaymentTermInMonth - 1));
+      let amortization = monthlyDue - interestOfMonth;
 
-tbody.innerHTML = "";
-
-  for (let i = 1; i <= repaymentTermInMonth; i++) {
-    let interestOfMonth = remainingBalance * interestPerMonth;
-    let monthlyDue = borrowed_amount * ((interestPerMonth * (1 + interestPerMonth) ** repaymentTermInMonth) / ((1 + interestPerMonth) ** repaymentTermInMonth - 1));
-    let amortization = monthlyDue - interestOfMonth;
-
-    remainingBalance -= amortization;
-    let row = `<tr>
+      remainingBalance -= amortization;
+      let row = `<tr>
             <td>${i}</td> 
             <td>${Math.round(balance)}€</td> 
             <td>${Math.round(monthlyDue)}€</td> 
@@ -39,33 +40,39 @@ tbody.innerHTML = "";
             <td>${Math.round(remainingBalance)}€</td> 
         </tr>`;
 
-    balance = remainingBalance;
-    tbody.innerHTML += row;
-  }
-   pdfButton.style.display = "block"
-}
-
-
-
-
-function checkNominalRate(){
-    if(nominal_rate_input.value <= 0){
-        console.log("no")
-        return false;
-    }else{
-        console.log("frr");
-        return true;
+      balance = remainingBalance;
+      tbody.innerHTML += row;
     }
+    pdfButton.style.display = "block";
+  } else {
+    tbody.innerHTML = "";
+    pdfButton.style.display = "none";
+  }
 }
 
-function checkBorrowedAmount(){
-  const borrowed_amount = Number(borrowed_amount_input.value);
-  if (Number.isInteger(borrowed_amount) && borrowed_amount > 0) {
-      console.log("Valid borrowed amount");
-      return true;
-  } else {
-      console.log("Invalid borrowed amount");
-      return false;
+function checkNominalRate() {
+  const nominal_rate_value = nominal_rate_input.value;
+  if (nominal_rate_value <= 0) {
+    console.log("Vous ne pouvez pas entrer une valeur inférieure à 0");
+    return false;
+  } else if (isNaN(nominal_rate_value)) {
+    console.log("Veuillez entrer une valeur numérique");
+    return false;
+  }else {
+    return true;
+  }
+}
+
+function checkBorrowedAmount() {
+  const borrowed_amount_value = borrowed_amount_input.value;
+  if (borrowed_amount_value <= 0) {
+    console.log("Vous ne pouvez pas entrer une valeur inférieure à 0");
+    return false;
+  } else if (isNaN(borrowed_amount_value)) {
+    console.log("Veuillez entrer une valeur numérique");
+    return false;
+  }else {
+    return true;
   }
 }
 
